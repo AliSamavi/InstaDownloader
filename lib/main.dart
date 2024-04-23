f8b7641e45f60570da2180bda5e1e7bd139c4824
Samavi <alisamav1213@gmail.com>
2024-04-23 19:59:34 +0330

    chore: Reset project to initial state
    Deleted all added files to start the project afresh.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:InstaDownloader/views/screens/download.dart';
-import 'package:InstaDownloader/views/screens/home.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MainApp());
 
@@ -10,25 +7,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.mulishTextTheme(
-          Theme.of(context).textTheme,
-        ),
-      ),
       home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: const Color(0xFF140F2D),
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF140F2D),
-          elevation: 0,
-        ),
-        body: const Stack(
-          children: [
-            HomeScreen(),
-            DownloadsScreen(),
-          ],
+        body: Center(
+          child: Text("Hello World!"),
         ),
       ),
     );
