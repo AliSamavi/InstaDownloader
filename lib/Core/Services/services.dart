1aad49e4ff0d458553ba4f471a301f3301b7ee11
Samavi <alisamav1213@gmail.com>
2024-06-01 02:43:46 +0330

    refactor(services): enhance error handling and simplify get method in Services
    Improved the get method in the Services class by removing redundant null returns and enhancing error handling. Now, the method returns a JSON error message for connection issues (SocketException) and a login error for other exceptions. Simplified the overall structure for better readability and reliability.

--- lib/Core/Services/services.dart
+++ lib/Core/Services/services.dart
@@ -2,26 +2,23 @@ import 'dart:convert';
 import 'dart:io';
 
 class Services {
-  static Future<Map<String, dynamic>?> get(
+  static Future<Map<String, dynamic>> get(
     String url,
     List<Cookie> cookies,
   ) async {
+    HttpClient client = HttpClient();
+
     try {
-      HttpClient client = HttpClient();
       HttpClientRequest req = await client.getUrl(Uri.parse(url));
-
       req.cookies.addAll(cookies);
-
       HttpClientResponse res = await req.close();
 
-      if (res.statusCode == 200) {
-        String json = await res.transform(utf8.decoder).join();
-        return jsonDecode(json);
-      }
+      String json = await res.transform(utf8.decoder).join();
+      return jsonDecode(json);
+    } on SocketException {
+      return {"error": "connection"};
     } catch (e) {
-      return null;
+      return {"error": "login"};
     }
-
-    return null;
   }
 }
